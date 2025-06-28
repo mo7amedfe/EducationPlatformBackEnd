4af66c2d7cd7ae663c22883b44d4fa194d9dcbee
mo7amedfe <mohamed223344631@gmail.com>
2025-06-28 19:45:18 +0300

    update

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-import express from 'express';
-const app = express();
-import { config } from 'dotenv';
-import { initapp } from './src/initapp.js';
-config();
+import express from 'express'
+const app = express()
+import { config } from 'dotenv'
+import { initapp } from './src/initapp.js'
+config()
+initapp(app,express)
+    export default app;
 
-// Initialize the app asynchronously
-const startApp = async () => {
-  try {
-    await initapp(app, express);
-  } catch (error) {
-    console.error('Error during app initialization:', error);
-  }
-};
 
-startApp();
 
-export default app;
