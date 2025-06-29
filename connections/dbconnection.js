ee94483cf4db889f1d9af457353248efe56ee3f8
mo7amedfe <mohamed223344631@gmail.com>
2025-06-29 14:47:06 +0300

    update

--- connections/dbconnection.js
+++ connections/dbconnection.js
@@ -1,8 +1,24 @@
 import mongoose from 'mongoose'
 
-export const connectionDB = async () => {
-  return await mongoose
-    .connect("mongodb://localhost:27017/grad-projecte")
-    .then((res) => console.log('DB connection success'))
-    .catch((err) => console.log('DB connection Fail', err))
-} 
+// export const connectionDB = async () => {
+//   return await mongoose
+//     .connect("mongodb://localhost:27017/grad-projecte")
+//     .then((res) => console.log('DB connection success'))
+//     .catch((err) => console.log('DB connection Fail', err))
+// } 
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      serverSelectionTimeoutMS: 10000,
+      bufferCommands: false,
+      dbName: 'test' // ✅ مهم تحدد اسم الداتا بيز لو مش محطوط في URI
+    });
+
+    console.log("✅ Connected to MongoDB");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error);
+    process.exit(1);
+  }
+};
+export default connectDB
--- index.js
+++ index.js
@@ -2,6 +2,15 @@ import express from "express";
 const app = express();
 import { config } from "dotenv";
 import { initapp } from "./src/initapp.js";
+import connectDB from './connections/dbconnection.js'; // حسب مكان الملف
 config();
+const startServer = async () => {
+    await connectDB(); // ✅ اتصل قبل أي استخدام للـ Models
+    app.listen(process.env.port, () => {
+      console.log(`🚀 Server is running on ${process.env.port}`);
+    });
+  };
+  
+  startServer();
 initapp(app, express);
 export default app;
